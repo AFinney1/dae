8006bdb9846e501331282cda1998ca00b77d9ede
afinney1 <amfinney792@gmail.com>
2021-07-27 23:40:24 -0500

    plotting load data

--- load_forecast.py
+++ load_forecast.py
@@ -12,18 +12,18 @@ from tensorflow import keras
 cwd = os.getcwd()
 
 '''Loading the Data'''
-def get_directories(path):
+def get_directories(path) -> list:
     """
     Returns a list of directories in the given path.
     """
     return [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
 
-def load_profile_df(filename):
+def load_profile_df(filename) -> pd.DataFrame:
     """
     Converts xls and xlsx files to pandas dataframe.
     """
     print(filename)
-    f = filename+'/'+os.listdir(filename)[0]
+    f = cwd+'/Profiles/'+filename+'/'+os.listdir(cwd+'/Profiles/'+filename)[0]
     print(f)
     if f.endswith('.xls') or f.endswith('.xlsx'):
         df = pd.read_excel(f)
@@ -34,17 +34,45 @@ def load_profile_df(filename):
 
 directories = get_directories('Profiles')
 directories.sort()
-#print(get_directories('Profiles'))
-#for d in directories:
-#    print(load_profile_df(d).head())
 
-'''Plot'''
-def plot_data(df):
+for d in directories:
+    print(d)
+    #print(load_profile_df(d).head())
+
+'''Plot/Evaluate Data'''
+def plot_load_data(df):
     """
-    Plot"""
+    Plot the load data for each day over time.
+    """
+    
+    original_columns = list(df.columns)
+    df.drop("ADDTIME", axis=1, inplace=True)
+    print(original_columns)
+    #original_columns[0] = "Station"
+    original_columns = ["Load at 15 min {}".format(i) for i in range(1, len(original_columns[2:]))]
+    original_columns.insert(0, "Station")
+    original_columns.insert(1, "Date")
+    #df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y')
+    df.columns = original_columns
+    #df.set_index('Date', inplace=True)
+    #df.T
+    #date_column = df['Date']
+    
+    df.plot(
+        y = original_columns[2:],
+        subplots = False
+    )#columns = date_column)
+    plt.title('Load Data')
+    plt.xlabel('Time (15 min. Intervals)')
+    plt.ylabel('Load (kWh)')
+    plt.show()
 
+    
+plot_load_data(load_profile_df(directories[0]))
 
-'''Data Evaluation/Statistics'''
+'''Prepare Data'''
+
+'''Preliminary Statistics'''
 def check_stationarity():
     """
     Check for constant mean and variance, and covariance is independent of time.
